dad879fc1b1846cc52bfb1174adcd31fb8982e1d
Lam Chan <lchan@xogrp.com>
2017-01-01 15:22:00 -0600

    refactored connection and channels to have smaller functions

--- test/integration.js
+++ test/integration.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const Async = require('async');
 const Code = require('code');
 const Lab = require('lab');
 
 const lab = exports.lab = Lab.script();
 const before = lab.before;
+const beforeEach = lab.beforeEach;
+const afterEach = lab.afterEach;
 const describe = lab.describe;
 const it = lab.it;
 const expect = Code.expect;
@@ -12,7 +15,7 @@ const expect = Code.expect;
 const BunnyBus = require('../lib');
 let instance = undefined;
 
-describe('integration', () => {
+describe('integration tests', () => {
 
     before((done) => {
 
@@ -21,14 +24,94 @@ describe('integration', () => {
         done();
     });
 
-    it('should connect with default values', (done) => {
+    describe('connection', () => {
 
-        instance.connect((err) => {
+        before((done) => {
 
-            expect(err).to.be.null();
-            expect(instance.connection).to.exist();
-            expect(instance.channel).to.exist();
-            done();
+            instance.closeConnection(done);
+        });
+
+        afterEach((done) => {
+
+            instance.closeConnection(done);
+        });
+
+        it('should create connection with default values', (done) => {
+
+            instance.createConnection((err) => {
+
+                expect(err).to.be.null();
+                expect(instance.connection).to.exist();
+                done();
+            });
+        });
+
+        it('should close an opened connection', (done) => {
+
+            Async.waterfall([
+                instance.createConnection.bind(instance),
+                instance.closeConnection.bind(instance)
+            ], (err) => {
+
+                expect(err).to.be.null();
+                expect(instance.connection).to.be.null();
+                done(err);
+            });
+        });
+    });
+
+    describe('channel', () => {
+
+        before((done) => {
+
+            instance.closeChannel(done);
+        });
+
+        beforeEach((done) => {
+
+            instance.createConnection(done);
+        });
+
+        afterEach((done) => {
+
+            instance.closeChannel(done);
+        });
+
+        it('should create channel with default values', (done) => {
+
+            instance.createChannel((err) => {
+
+                expect(err).to.be.null();
+                expect(instance.channel).to.exist();
+                done();
+            });
+        });
+
+        it('should close an opened channel', (done) => {
+
+            Async.waterfall([
+                instance.createChannel.bind(instance),
+                instance.closeChannel.bind(instance)
+            ], (err) => {
+
+                expect(err).to.be.null();
+                expect(instance.channel).to.be.null();
+                done(err);
+            });
+        });
+
+        it('should close both connection and channel when closing a connection', (done) => {
+
+            Async.waterfall([
+                instance.createChannel.bind(instance),
+                instance.closeConnection.bind(instance)
+            ], (err) => {
+
+                expect(err).to.be.null();
+                expect(instance.connection).to.be.null();
+                expect(instance.channel).to.be.null();
+                done(err);
+            });
         });
     });
 });
