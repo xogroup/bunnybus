18a4813a36513c925270bcebde71bed309ffb2c7
Lam Chan <lchan@tenna.com>
2020-01-24 10:16:12 -0600

    added isString helper

--- test/node6/helpers.js
+++ test/node6/helpers.js
@@ -770,4 +770,50 @@ describe('helpers', () => {
             expect(result).to.have.length(2);
         });
     });
+
+    describe('isString', () => {
+
+        it('should be true with string literal', () => {
+
+            const result = Helpers.isString('foo');
+
+            expect(result).to.be.true();
+        });
+
+        it('should be true with string object', () => {
+
+            // eslint-disable-next-line no-new-wrappers
+            const result = Helpers.isString(new String('foo'));
+
+            expect(result).to.be.true();
+        });
+
+        it('should be false when undefined', () => {
+
+            const result = Helpers.isString();
+
+            expect(result).to.be.false();
+        });
+
+        it('should be false for a simple object', () => {
+
+            const result = Helpers.isString({});
+
+            expect(result).to.be.false();
+        });
+
+        it('should be false for a complex object', () => {
+
+            const result = Helpers.isString(new Date());
+
+            expect(result).to.be.false();
+        });
+
+        it('should be false for a function', () => {
+
+            const result = Helpers.isString(() => {});
+
+            expect(result).to.be.false();
+        });
+    });
 });
