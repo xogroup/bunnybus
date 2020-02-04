cc5452cf8810243db1687c6945cd638ec2b22a83
Lam Chan <lchan@tenna.com>
2020-02-04 14:08:52 +0800

    added channel manager

---
+++ test/node6/states/channelManager.js
@@ -0,0 +1,224 @@
+'use strict';
+
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+const BunnyBus = require('../../../lib');
+const { ChannelManager, ConnectionManager } = require('../../../lib/states');
+const Exceptions = require('../../../lib/exceptions');
+
+const { describe, beforeEach, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+describe('state management', () => {
+
+    describe('Channel Manager', () => {
+
+        let instance = undefined;
+        let connectionContext = undefined;
+        let defaultConfiguration = undefined;
+
+        beforeEach(async () => {
+
+            defaultConfiguration = BunnyBus.DEFAULT_SERVER_CONFIGURATION;
+            connectionContext = await (new ConnectionManager()).create('channel-baseConnection', defaultConfiguration);
+            instance = new ChannelManager();
+        });
+
+        describe('create', () => {
+
+            const baseChannelName = 'channel-createChannel';
+
+            it('should create a channel with default values', async () => {
+
+                await instance.create(baseChannelName, connectionContext, defaultConfiguration);
+
+                const result = instance._channels.get(baseChannelName);
+
+                expect(result).to.exist();
+                expect(result).to.be.an.object();
+                expect(result.channelOptions).to.exist();
+                expect(result.channelOptions).to.contain(defaultConfiguration);
+                expect(result.channel).to.exist();
+                expect(result.channel).to.be.an.object();
+            });
+
+            it('should return same channel when request are called concurrently', async () => {
+
+                const [result1, result2] = await Promise.all([
+                    instance.create(baseChannelName, connectionContext, defaultConfiguration),
+                    instance.create(baseChannelName, connectionContext, defaultConfiguration)
+                ]);
+
+                expect(result1).to.exist();
+                expect(result2).to.exist();
+                expect(result1).to.shallow.equal(result2);
+            });
+
+            it('should return same connection when request are called sequentially', async () => {
+
+                const result1 = await instance.create(baseChannelName, connectionContext, defaultConfiguration);
+                const result2 = await instance.create(baseChannelName, connectionContext, defaultConfiguration);
+
+                expect(result1).to.exist();
+                expect(result2).to.exist();
+                expect(result1).to.shallow.equal(result2);
+                expect(result1.channel).to.shallow.equal(result2.channel);
+            });
+
+            it('should error when no connectionContext is supplied', async () => {
+
+                let sut = null;
+
+                try {
+                    await instance.create(baseChannelName);
+                }
+                catch (err) {
+                    sut = err;
+                }
+
+                expect(sut).to.exist();
+                expect(sut).to.be.an.error(Exceptions.NoConnectionError);
+            });
+
+            it('should error when connectionContext supplied does not have a connection', async () => {
+
+                connectionContext.connection = undefined;
+
+                let sut = null;
+
+                try {
+                    await instance.create(baseChannelName, connectionContext);
+                }
+                catch (err) {
+                    sut = err;
+                }
+
+                expect(sut).to.exist();
+                expect(sut).to.be.an.error(Exceptions.NoConnectionError);
+            });
+
+            it('should error when no channel options is supplied', async () => {
+
+                let sut = null;
+
+                try {
+                    await instance.create(baseChannelName, connectionContext);
+                }
+                catch (err) {
+                    sut = err;
+                }
+
+                expect(sut).to.exist();
+                expect(sut).to.be.an.error('Expected channelOptions to be supplied');
+            });
+
+            it('should error when a confirming channel call fails', { timeout: 10000 }, async () => {
+
+                connectionContext.connection.createConfirmChannel = async () => {
+                    
+                    throw new Error('Mock error');
+                };
+
+                let sut = null;
+
+                try {
+                    await instance.create(baseChannelName, connectionContext, { prefetch: 1, connectionRetryCount: 1, timeout: 100 });
+                }
+                catch (err) {
+                    sut = err;
+                }
+
+                expect(sut).to.exist();
+                expect(sut).to.be.an.error('Exceeded maximum attempts of retries');
+            });
+        });
+
+        describe('contains', () => {
+
+            const baseChannelName = 'channel-containsChannel';
+
+            it('should return true when channel context exist', async () => {
+
+                await instance.create(baseChannelName, connectionContext, defaultConfiguration);
+
+                const result = instance.contains(baseChannelName);
+
+                expect(result).to.be.true();
+            });
+
+            it('should return false when connection context does not exist', async () => {
+
+                const result = instance.contains(baseChannelName);
+
+                expect(result).to.be.false();
+            });
+        });
+
+        describe('get', () => {
+
+            const baseChannelName = 'channel-getChannel';
+
+            it('should return a connection context when it exist', async () => {
+
+                const channelContext = await instance.create(baseChannelName, connectionContext, defaultConfiguration);
+
+                const result = instance.get(baseChannelName);
+
+                expect(result).to.exist();
+                expect(result.name).to.equal(baseChannelName);
+                expect(result.channelOptions).to.contains(defaultConfiguration);
+                expect(result.channel).to.shallow.equal(channelContext.channel);
+            });
+
+            it('should be undefined when the connection context does not exist', async () => {
+
+                const result = instance.get(baseChannelName);
+
+                expect(result).to.not.exist();
+                expect(result).to.be.undefined();
+            });
+        });
+
+        describe('getChannel', () => {
+
+            const baseChannelName = 'channel-getChannelChannel';
+
+            it('should return a channel when it exist', async () => {
+
+                const channelContext = await instance.create(baseChannelName, connectionContext, defaultConfiguration);
+
+                const result = instance.getChannel(baseChannelName);
+
+                expect(result).to.exist();
+                expect(result).to.shallow.equal(channelContext.channel);
+            });
+
+            it('should be undefined when the channel does not exist', async () => {
+
+                const result = instance.get(baseChannelName);
+
+                expect(result).to.not.exist();
+                expect(result).to.be.undefined();
+            });
+        });
+
+        describe('close', () => {
+
+            const baseChannelName = 'channel-closeChannel';
+
+            it('should close channel when it exist', async () => {
+
+                await instance.create(baseChannelName, connectionContext, defaultConfiguration);
+
+                await instance.close(baseChannelName);
+
+                const result = instance._channels.get(baseChannelName);
+
+                expect(result).to.exist();
+                expect(result.name).to.equal(baseChannelName);
+                expect(result.channel).to.not.exist();
+                expect(result.channel).to.be.undefined();
+            });
+        });
+    });
+});
---
+++ test/node6/states/connectionManager.js
@@ -0,0 +1,212 @@
+'use strict';
+
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+const BunnyBus = require('../../../lib');
+const { ConnectionManager } = require('../../../lib/states');
+
+const { describe, beforeEach, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+describe('state management', () => {
+
+    describe('Connection Manager', () => {
+
+        let instance = undefined;
+        let defaultConfiguration = undefined;
+
+        beforeEach(() => {
+
+            instance = new ConnectionManager();
+            defaultConfiguration = BunnyBus.DEFAULT_SERVER_CONFIGURATION;
+        });
+
+        describe('create', () => {
+
+            const baseConnectionName = 'connection-createConnection';
+
+            it('should create a connection with default values', async () => {
+
+                await instance.create(baseConnectionName, defaultConfiguration);
+
+                const result = instance._connections.get(baseConnectionName);
+
+                expect(result).to.exist();
+                expect(result).to.be.an.object();
+                expect(result.connectionOptions).to.exist();
+                expect(result.connectionOptions).to.contain(defaultConfiguration);
+                expect(result.connection).to.exist();
+                expect(result.connection).to.be.an.object();
+            });
+
+            it('should create a connection with defaults values while supplied with empty net/tls options', async () => {
+
+                await instance.create(baseConnectionName, defaultConfiguration, {});
+
+                const result = instance._connections.get(baseConnectionName);
+
+                expect(result).to.exist();
+                expect(result).to.be.an.object();
+                expect(result.connectionOptions).to.exist();
+                expect(result.connectionOptions).to.contain(defaultConfiguration);
+                expect(result.connection).to.exist();
+                expect(result.connection).to.be.an.object();
+            });
+
+            it('should create a connection with defaults values while supplied with partially filled net/tls options', async () => {
+
+                await instance.create(baseConnectionName, defaultConfiguration, { allowHalfOpen: true });
+
+                const result = instance._connections.get(baseConnectionName);
+
+                expect(result).to.exist();
+                expect(result).to.be.an.object();
+                expect(result.connectionOptions).to.exist();
+                expect(result.connectionOptions).to.contain(defaultConfiguration);
+                expect(result.connection).to.exist();
+                expect(result.connection).to.be.an.object();
+            });
+
+            it('should return same connection when request are called concurrently', async () => {
+
+                const [result1, result2] = await Promise.all([
+                    instance.create(baseConnectionName, defaultConfiguration),
+                    instance.create(baseConnectionName, defaultConfiguration)
+                ]);
+
+                expect(result1).to.exist();
+                expect(result2).to.exist();
+                expect(result1).to.shallow.equal(result2);
+            });
+
+            it('should return same connection when request are called sequentially', async () => {
+
+                const result1 = await instance.create(baseConnectionName, defaultConfiguration);
+                const result2 = await instance.create(baseConnectionName, defaultConfiguration);
+
+                expect(result1).to.exist();
+                expect(result2).to.exist();
+                expect(result1).to.shallow.equal(result2);
+                expect(result1.connection).to.shallow.equal(result2.connection);
+            });
+
+            it('should error when no connection options is supplied', async () => {
+
+                let sut = null;
+
+                try {
+                    await instance.create(baseConnectionName);
+                }
+                catch (err) {
+                    sut = err;
+                }
+
+                expect(sut).to.exist();
+                expect(sut).to.be.an.error('Expected connectionOptions to be supplied');
+            });
+
+            it('should error when a misconfigured object is supplied', { timeout: 10000 }, async () => {
+
+                let sut = null;
+
+                try {
+                    await instance.create(baseConnectionName, { port: 60000, connectionRetryCount: 1 });
+                }
+                catch (err) {
+                    sut = err;
+                }
+
+                expect(sut).to.exist();
+                expect(sut).to.be.an.error('Exceeded maximum attempts of retries');
+            });
+        });
+
+        describe('contains', () => {
+
+            const baseConnectionName = 'connection-containsConnection';
+
+            it('should return true when connection context exist', async () => {
+
+                await instance.create(baseConnectionName, defaultConfiguration);
+
+                const result = instance.contains(baseConnectionName);
+
+                expect(result).to.be.true();
+            });
+
+            it('should return false when connection context does not exist', async () => {
+
+                const result = instance.contains(baseConnectionName);
+
+                expect(result).to.be.false();
+            });
+        });
+
+        describe('get', () => {
+
+            const baseConnectionName = 'connection-getConnection';
+
+            it('should return a connection context when it exist', async () => {
+
+                const connectionContext = await instance.create(baseConnectionName, defaultConfiguration);
+
+                const result = instance.get(baseConnectionName);
+
+                expect(result).to.exist();
+                expect(result.name).to.equal(baseConnectionName);
+                expect(result.connectionOptions).to.contains(defaultConfiguration);
+                expect(result.connection).to.shallow.equal(connectionContext.connection);
+            });
+
+            it('should be undefined when the connection context does not exist', async () => {
+
+                const result = instance.get(baseConnectionName);
+
+                expect(result).to.not.exist();
+                expect(result).to.be.undefined();
+            });
+        });
+
+        describe('getConnection', () => {
+
+            const baseConnectionName = 'connection-getConnectionConnection';
+
+            it('should return a connection when it exist', async () => {
+
+                const connectionContext = await instance.create(baseConnectionName, defaultConfiguration);
+
+                const result = instance.getConnection(baseConnectionName);
+
+                expect(result).to.exist();
+                expect(result).to.shallow.equal(connectionContext.connection);
+            });
+
+            it('should be undefined when the connection does not exist', async () => {
+
+                const result = instance.get(baseConnectionName);
+
+                expect(result).to.not.exist();
+                expect(result).to.be.undefined();
+            });
+        });
+
+        describe('close', () => {
+
+            const baseConnectionName = 'connection-closeConnection';
+
+            it('should close connection when it exist', async () => {
+
+                await instance.create(baseConnectionName, defaultConfiguration);
+
+                await instance.close(baseConnectionName);
+
+                const result = instance._connections.get(baseConnectionName);
+
+                expect(result).to.exist();
+                expect(result.name).to.equal(baseConnectionName);
+                expect(result.connection).to.not.exist();
+                expect(result.connection).to.be.undefined();
+            });
+        });
+    });
+});
