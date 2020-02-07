06200b8895ffc41ef53fe020f06d538c1b42c2a9
Lam Chan <lchan@tenna.com>
2020-02-07 14:57:39 +0800

    added queue

---
+++ test/new/BunnyBus/createQueue.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+const Assertions = require('../assertions');
+const BunnyBus = require('../../../lib');
+
+const { describe, before, beforeEach, after, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+let instance = undefined;
+let connectionContext = undefined;
+let channelManager = undefined;
+let channelContext = undefined;
+
+describe('BunnyBus', () => {
+
+    before(() => {
+
+        instance = new BunnyBus();
+        instance.config = BunnyBus.DEFAULT_SERVER_CONFIGURATION;
+        channelManager = instance.channels;
+    });
+
+    describe('public methods', () => {
+
+        describe.only('createQueue', () => {
+
+            const baseChannelName = 'bunnybus-createQueue';
+            const baseQueueName = 'test-queue';
+
+            beforeEach(async () => {
+
+                channelContext = await instance._autoBuildChannelContext(baseChannelName);
+                connectionContext = channelContext.connectionContext;
+
+                await channelContext.channel.deleteQueue(baseQueueName);
+            });
+
+            after(async () => {
+
+                await channelContext.channel.deleteQueue(baseQueueName);
+            });
+
+            it(`should create a queue with name ${baseQueueName}`, async () => {
+
+                await Assertions.autoRecoverChannel(async () => {
+
+                    let result = null;
+
+                    await instance.createQueue(baseQueueName);
+
+                    try {
+                        await channelContext.channel.checkQueue(baseQueueName);
+                    }
+                    catch (err) {
+                        result = err;
+                    }
+
+                    expect(result).to.not.exist();
+                },
+                connectionContext,
+                channelContext,
+                channelManager);
+            });
+
+            it('should not error when creating queue concurrently', async () => {
+
+                await Assertions.autoRecoverChannel(async () => {
+
+                    let result = null;
+
+                    await Promise.all([
+                        instance.createQueue(baseQueueName),
+                        instance.createQueue(baseQueueName)
+                    ]);
+
+                    try {
+                        await channelContext.channel.checkQueue(baseQueueName);
+                    }
+                    catch (err) {
+                        result = err;
+                    }
+
+                    expect(result).to.not.exist();
+                },
+                connectionContext,
+                channelContext,
+                channelManager);
+            });
+
+            it('should not error when creating queue sequentially', async () => {
+
+                await Assertions.autoRecoverChannel(async () => {
+
+                    let result = null;
+
+                    await instance.createQueue(baseQueueName),
+                    await instance.createQueue(baseQueueName);
+
+                    try {
+                        await channelContext.channel.checkQueue(baseQueueName);
+                    }
+                    catch (err) {
+                        result = err;
+                    }
+
+                    expect(result).to.not.exist();
+                },
+                connectionContext,
+                channelContext,
+                channelManager);
+            });
+        });
+    });
+});
