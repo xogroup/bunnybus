abd76a9d3e1a69e9b4fe5b742f7931ea077ca5aa
Lam Chan <lchan@tenna.com>
2021-02-10 11:56:51 -0800

    added load test and support for realtime config update

--- test/lab/BunnyBus/end-to-end/load.js
+++ test/lab/BunnyBus/end-to-end/load.js
@@ -98,6 +98,95 @@ describe('BunnyBus', () => {
             });
         });
 
+        describe('load test with partitioned serial dispatcher', () => {
+            const baseChannelName = 'bunnybus-e2e-load-partitioned-serial-test';
+            const baseQueueName = 'test-e2e-load-partitioned-serial-test-queue';
+            const baseErrorQueueName = `${baseQueueName}_error`;
+            const message = { event: 'a.promise', name: 'bunnybus' };
+            const pattern = 'a.promise';
+            const partitionKeySelector = '{message.serialNumber}';
+            const partitionCount = 10;
+            const publishTarget = 300;
+
+            before(async () => {
+                instance = new BunnyBus();
+                instance.config = Object.assign({}, BunnyBus.DEFAULT_SERVER_CONFIGURATION, {
+                    dispatchType: 'partitionSerial',
+                    serialDispatchPartitionKeySelectors: [partitionKeySelector]
+                });
+                connectionManager = instance.connections;
+                channelManager = instance.channels;
+
+                channelContext = await instance._autoBuildChannelContext({ channelName: baseChannelName });
+
+                await Promise.all([
+                    channelContext.channel.deleteExchange(instance.config.globalExchange),
+                    channelContext.channel.deleteQueue(baseQueueName),
+                    channelContext.channel.deleteQueue(baseErrorQueueName)
+                ]);
+
+                await Promise.all([
+                    channelContext.channel.assertExchange(instance.config.globalExchange, 'topic'),
+                    channelContext.channel.assertQueue(baseQueueName, { durable: false })
+                ]);
+
+                await channelContext.channel.bindQueue(baseQueueName, instance.config.globalExchange, pattern);
+            });
+
+            afterEach(async () => {
+                await instance.unsubscribe({ queue: baseQueueName });
+            });
+
+            after(async () => {
+                await Promise.all([
+                    channelContext.channel.deleteExchange(instance.config.globalExchange),
+                    channelContext.channel.deleteQueue(baseQueueName),
+                    channelContext.channel.deleteQueue(baseErrorQueueName)
+                ]);
+
+                await instance.stop();
+            });
+
+            it('should publish all messages within 3 seconds', { timeout: 3000 }, async () => {
+                const promises = [];
+
+                // Do this to prime the connection
+                await instance.publish({ message });
+
+                for (let i = 0; i < publishTarget; ++i) {
+                    promises.push(instance.publish({ message: { ...message, serialNumber: `${i % partitionCount}` } }));
+                }
+
+                await Promise.all(promises);
+            });
+
+            it('should consume all messages within 5 seconds', { timeout: 5000 }, async () => {
+                let count = 0;
+
+                const handlers = {};
+
+                const promise = new Promise(async (resolve) => {
+                    handlers['a.promise'] = async ({ ack }) => {
+                        await ack();
+
+                        if (++count === publishTarget) {
+                            resolve();
+                        }
+                    };
+                });
+
+                await instance.subscribe({
+                    queue: baseQueueName,
+                    handlers,
+                    options: {
+                        queue: { durable: false }
+                    }
+                });
+
+                await promise;
+            });
+        });
+
         describe('load test with concurrent dispatcher', () => {
             const baseChannelName = 'bunnybus-e2e-load-concurrent-test';
             const baseQueueName = 'test-e2e-load-concurrent-test-queue';
